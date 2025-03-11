956d34b140dfd8e0fd9b94b9a64675d38c79d232
Aizaz <kotwal.aizaz24@gmail.com>
2025-03-12 02:07:56 +0530

    feat: add nav components

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from './ui/button'
+
+const MainNav = () => {
+  return (
+    <Button variant={"ghost"} className='font-bold hover:text-orange-500 hover:bg-orange-200'>LOG IN</Button>
+  )
+}
+
+export default MainNav
