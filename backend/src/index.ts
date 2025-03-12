df07b7fe02f4c5e271cca028fba48fac80c0b337
Aizaz <kotwal.aizaz24@gmail.com>
2025-03-12 15:49:48 +0530

    feat: add user route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRouter from "./routes/UserRoutes";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("connected to database!")
@@ -9,11 +10,7 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
 const app = express();
 app.use(express.json()); //? automatically converts body of any request made to the server into JSON.
 app.use(cors());
-
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello World!" });
-});
-
+app.use("/api/user", userRouter)
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
 });
