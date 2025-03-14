c188859a5358ae5167970f605ecb698a957fcbca
Aizaz <kotwal.aizaz24@gmail.com>
2025-03-14 17:40:01 +0530

    chore: add cloudinary config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 import userRouter from "./routes/user.routes";
 // import { createUser } from "./controllers/user.controller";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("connected to database!");
 });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 const app = express();
 app.use(express.json()); //? automatically converts body of any request made to the server into JSON.
 app.use(cors());
